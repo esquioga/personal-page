aaf8ce75ceefe8acb93eebaa02d086fb6c9eb30c
João Esquioga <joao.esquioga@thera.com.br>
2020-08-04 23:11:44 -0300

    added initial layout header + main content + sub section title

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
   <v-app>
-    <v-container fluid>
-      <v-card>
+    <v-container>
+      <v-card elevation="10" dark>
         <v-card-title> João Esquioga </v-card-title>
         <v-card-subtitle> Fullstack Developer </v-card-subtitle>
+        <v-divider/>
+        <v-card-text>
+          <v-container grid-list-md text-xs-center>
+            <v-layout row justify-space-around align-center>
+              <v-flex xs4>
+                <v-img width="240px" height="240px" src="./assets/img/avatar-placeholder.png"/>
+              </v-flex>
+              <v-flex xs5>
+                <v-card elevation="0">
+                  <v-card-title>Lorem Ipsum</v-card-title>
+                  <v-card-text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porta neque in urna varius, ut dictum ligula pretium. Sed non orci massa. Pellentesque volutpat lorem arcu, ac egestas felis varius sit amet. Etiam pharetra ex vel nulla venenatis, eget gravida metus fringilla. Cras congue ullamcorper tempor. Ut mollis bibendum risus vitae posuere. Integer tincidunt in ligula eget ultricies. Suspendisse condimentum lobortis euismod. Duis augue tellus, volutpat eget dolor sit amet, tempus sodales elit. Vivamus id lacinia lectus, eu consequat ante.
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>          
+        </v-card-text>
+        <v-card-title class="section-title">What I do </v-card-title>
+        <div class="divider"/>
       </v-card>
     </v-container>
   </v-app>
@@ -17,3 +37,24 @@ export default {
   }),
 };
 </script>
+<style lang="scss" scoped>
+
+.v-application {
+  background-color: #F5F5F5 !important;
+}
+
+.container {
+  .v-card {
+    .section-title {
+      padding: 0px 16px;
+    }
+    .divider {
+      width: 36px;
+      margin: 0px 16px;
+      padding: 0px;
+      border-bottom: 1px solid #F5F5F5;
+    }
+  }
+}
+
+</style>
